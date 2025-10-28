b7e6e14e70b8e826854a705ef8ec4671947f87c0
benjvdev <benja.casta04@gmail.com>
2025-10-28 14:38:40 -0300

    login incompleto

---
+++ app/src/main/java/com/example/levelupmobile/ui/screens/LoginScreen.kt
@@ -0,0 +1,80 @@
+package com.example.levelupmobile.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.levelupmobile.R
+
+@Composable
+fun LoginScreen(){
+    Box(Modifier.fillMaxSize().padding(16.dp)){
+        Login(Modifier.align(Alignment.Center))
+    }
+}
+
+@Composable
+fun Login(modifier: Modifier) {
+    Column(modifier = modifier) {
+        HeaderImage(Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.padding(16.dp))
+        EmailField()
+        Spacer(modifier = Modifier.padding(4.dp))
+        PasswordField()
+        Spacer(modifier = Modifier.padding(8.dp))
+    }
+}
+
+@Composable
+fun PasswordField() {
+    TextField(
+        value = "",
+        onValueChange = {},
+        placeholder = {Text(text="Contraseña")},
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    )
+}
+
+@Composable
+fun EmailField() {
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Email") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    )
+}
+
+@Composable
+fun HeaderImage(modifier: Modifier) {
+    Image(painter = painterResource(id = R.drawable.levelup), contentDescription = "Logo", modifier = modifier)
+}
